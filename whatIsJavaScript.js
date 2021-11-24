f720a58a4f3e9ce701b0d22c98f0bef9c31f4627
Tom Sanderson <tom.w.j.sanderson@gmail.com>
2021-11-23 22:43:52 -0500

    add more files

--- whatIsJavaScript.js
+++ whatIsJavaScript.js
@@ -1,6 +1,18 @@
 /**
  * So what is JavaScript and why should I learn it?
  * 
- * This is a great question. JavaScript is a hugely popular programming language used all over the world. 
- * It was originally built 
+ * This is a great question. JavaScript is a hugely popular programming language that is used all over the world. 
+ * It was originally built by a man named Brendon Eich for a company called Netscape back in 1995.
+ * 
+ * It was intended to run inside a web browser and give websites dynamic behaviour in contrast to static websites of the early 90's.
+ * 
+ * Some examples of the dynamic behaviour JavaScript produces are:
+ * 
+ * - Loading new web page content without reloading the page
+ * - Web page animations, such as fading objects in and out
+ * - Playing games in the browsers
+ * - Authenticating users with a backend server
+ * 
+ * 
+ * 
  */
