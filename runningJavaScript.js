881da7839b446ee47611781bd2f074019285a6f9
Tom Sanderson <tom.w.j.sanderson@gmail.com>
2021-11-25 23:51:47 -0500

    finish what is js, add running js

---
+++ runningJavaScript.js
@@ -0,0 +1,7 @@
+/**
+ * setup node
+ * run in browser
+ * run in free applications on line
+ * run in node
+ * run files from node
+ */
--- whatIsJavaScript.js
+++ whatIsJavaScript.js
@@ -19,8 +19,16 @@
  * This meant that we could only run JavaScript when someone called a server (ie. entered a URL address) and was sent a bunch of HTML/CSS & JavaScript files
  * that would be loaded into and then run in your browser.
  * 
- * Eventually JavaScript picked up in popularity and 
+ * In 2009 a guy called Ryan Dahl created a runtime environment that would allow developers to execute JavaScript code outside of the browser. 
+ * This opened up a world of new possibilities for developers to start using JavaScript as a backend language the could be used to build
+ * servers and even interact with the operating system.
  * 
+ * As of 2022 and the creation of this video, JavaScript is now a well supported langauge with thousands of libraries, framework ands applications
+ * to choose from. You can do everything from building instance chat apps, building Machine Learning models, building apps on various blockchains 
+ * and even doing 3D rendering.
+ * 
+ * In the next video I will walk you through setting up Node.js on your own computer and in various environments so you can start running JavaScript
+ * code your self.
  * 
  * 
  */
