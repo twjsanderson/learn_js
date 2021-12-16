8475b2f0dc7b9eaff211044066d2abb82cea06ce
Tom Sanderson <tom.w.j.sanderson@gmail.com>
2021-12-15 22:43:34 -0500

    commandline

---
+++ commandLine.js
@@ -0,0 +1,22 @@
+/**
+ * What is the command prompt?
+ * 
+ * An interactive program that allows you to interact with your computer through written commands. Back when computers
+ * were firts created there wasn't a graphical user interface or UI or GUI to point and click on visual elements in 
+ * your operating system. Everything was done by writing in a command prompt. Even to this day it is often faster to execute
+ * certain commands via the command prompt than it is by clicking through several screens to do something. Especially when
+ * programming. Many pieces of software, libraries and frameworks you will use to bulding web applications will accessed 
+ * driectly through the command line.
+ * 
+ * In windows, you have access to the command prompt or shell in various ways. 
+ * 
+ * Type "command prompt" into the Start menu to search for it. You can also type "cmd" (the short name of the executable that runs the Command Prompt) if you prefer.
+ * Press Win + R to open the Run box, then type "cmd" and hit Enter to open it.
+ * Press Win + X (or right-click the Start button) and choose Command Prompt from the menu. Depending on your Windows settings, this may show Windows PowerShell instead. 
+ * PowerShell is more powerful than the Command Prompt, but can run all the same commands.
+ * 
+ * Powershell is the name of the modern command line application that is native to Microsoft windows operating systems, at leat modern
+ * versions. It has a similar but different syntax to operating system like Linux, which uses Bash. 
+ * 
+ * The Mac command line is called the Terminal. Again the syntax is a bit different from the other 2 but it achieves the same thing.
+ */
